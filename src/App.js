d54ad9a66ec28ca72ee8060a8557bebbb119d615
Algirdas Stricka <algirdasstricka@yahoo.com>
2021-11-23 08:57:51 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyBtn from './components/MyBtn';
+
+function App() {
+  return (
+    <div>
+      <h2>App</h2>
+      <MyBtn></MyBtn>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyBtn.js
@@ -0,0 +1,4 @@
+function MyBtn() {
+  return <button>Click me</button>;
+}
+export default MyBtn;
